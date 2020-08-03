1b4fc5635e26fd4de1ce5061ebe23eff6b1558b5
MatiasLMontechiari <matias.monte@davinci.edu.ar>
2020-08-03 16:19:18 -0300

    envio de mail implementado

--- enviomail.php
+++ enviomail.php
@@ -1,33 +1,33 @@
 <?php
-    // Get the form fields, removes html tags and whitespace.
+    //Se toman los compos del formulario eliminando el contenidod del html
     $nombre = strip_tags(trim($_POST["nombre"]));
     $nombre = str_replace(array("\r","\n"),array(" "," "),$nombre);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $mensaje = trim($_POST["mensaje"]);
 
-    // Check the data.
+    // Se valida el contenido de los datos y en caso de error se dirige a una pagina de error
     if (empty($nombre) OR empty($mensaje) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         header("Location: https://miyashimatravel.netlify.app/contacto.html");
         exit;
     }
 
-    // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "contact@miyashimatravel.com";
+    // Se reciben los mail aca
+    $recipient = "matias.monte@davinci.edu.ar";
 
-    // Set the email subject.
+    // Establecemos el asunto del mail
     $subject = "New contact from $nombre";
 
-    // Build the email content.
+    // Establecemos el cuerpo del mail
     $email_content = "nombre: $nombre\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "mensaje:\n$mensaje\n";
 
-    // Build the email headers.
+    // Establecemose el encabezado del correo
     $email_headers = "From: $nombre <$email>";
 
-    // Send the email.
+    // funcion de envio mails php
     mail($recipient, $subject, $email_content, $email_headers);
     
-    // Redirect to the index.html page with success code
+    // Se re dirige a la seccion gracias cuando se realizo el envio del mail correctamente
     header("Location: https://miyashimatravel.netlify.app/index.html");
 ?>
